91cc12c34179da512b90f5147061145412f78ef0
Nour-hash <113339425+Nour-hash@users.noreply.github.com>
2025-04-06 00:39:14 +0200

    added InvitePage.jsx and changed navigation and home button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
-import { jwtDecode } from "jwt-decode";
 import './App.css';
 import "./styles/Login.css"; // Import a CSS file for styling
 import Register from "./pages/Register"; // Import the Register component
 import Home from "./pages/Home"; // Import the Register component
+import InvitePage from "./pages/InvitePage"; // hinzufügen
+
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/invite" element={<InvitePage />} />
       </Routes>
     </Router>
   );
@@ -42,7 +44,7 @@ function Login() {
       if (response.ok) {
         const token = data.replace("JWT Token: ", "");
         localStorage.setItem("jwt", token);
-        navigate("/hello");
+        navigate("/home");
       } else {
         setError(data);
       }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../styles/Home.css";
 
 function Home() {
-  return (
+
+    const navigate = useNavigate();
+
+    const handleWeiter = () => {
+        navigate("/invite"); // ✅ leitet weiter
+    };
+
+    return (
     <div className="home-container">
       <div className="home-box">
         <h1 className="home-title">Willkommen auf der Startseite</h1>
         <p className="home-text">
           Sie haben sich erfolgreich angemeldet und Ihre GmbH wurde bestätigt.
         </p>
-        <button className="home-button">Weiter</button>
+        <button className="home-button" onClick={handleWeiter}>Einladung erstellen</button>
       </div>
     </div>
   );
